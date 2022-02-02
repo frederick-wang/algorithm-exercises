652224fda4f19d3a292d667d49fc2b82d4632644
Zhaoji Wang <hwoam@outlook.com>
2022-02-02 11:04:32 +0800

    洛谷: P5724 (new)

--- luogu/P5724 【深基4.习5】求极差 - 最大跨度值/test.py
+++ luogu/P5724 【深基4.习5】求极差 - 最大跨度值/test.py
@@ -1,14 +1,6 @@
 # P5724 【深基4.习5】求极差 / 最大跨度值 https://www.luogu.com.cn/problem/P5724
 
 n = int(input())
+A = list(map(int, input().split()))
 
-a_min = 1000
-a_max = 0
-
-for a in map(int, input().split()):
-    if a < a_min:
-        a_min = a
-    if a > a_max:
-        a_max = a
-
-print(a_max - a_min)
+print(max(A) - min(A))

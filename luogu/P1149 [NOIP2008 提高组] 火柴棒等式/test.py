5b492471bce8a019ba762d124e8efba416d5d0d4
Zhaoji Wang <hwoam@outlook.com>
2022-02-27 22:59:34 +0800

    洛谷: P1149

--- luogu/P1149 [NOIP2008 提高组] 火柴棒等式/test.py
+++ luogu/P1149 [NOIP2008 提高组] 火柴棒等式/test.py
@@ -1,33 +1,29 @@
 # P1149 [NOIP2008 提高组] 火柴棒等式 https://www.luogu.com.cn/problem/P1149
-# https://www.luogu.com.cn/record/70260851
-
-from typing import Dict
+# https://www.luogu.com.cn/record/70261238
 
 N = int(input())
 MAX_NUM = N - 4
 
 matches = (6, 2, 5, 5, 4, 5, 6, 3, 7, 6)
-matches_num: Dict[int, int] = {}
+matches_num = [0] * 1999
 result = 0
 
 
 def get_matches_num(n: int):
-    if n not in matches_num:
+    if not matches_num[n]:
         matches_num[n] = sum(map(matches.__getitem__, map(int, str(n))))
     return matches_num[n]
 
 
-for a in range(1000):
-    a_num = get_matches_num(a)
+nums = sorted(map(lambda x: (get_matches_num(x), x), range(1000)))
+for a_num, a in nums:
     if a_num > MAX_NUM:
-        continue
-    for b in range(1000):
-        b_num = get_matches_num(b)
+        break
+    for b_num, b in nums:
         if a_num + b_num > MAX_NUM:
-            continue
+            break
         c = a + b
         c_num = get_matches_num(c)
         if a_num + b_num + c_num == MAX_NUM:
             result += 1
-
 print(result)

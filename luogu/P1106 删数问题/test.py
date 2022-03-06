e21767fd5f54169ea709030ac86c8a1bb31afadc
Zhaoji Wang <hwoam@outlook.com>
2022-03-07 00:01:21 +0800

    洛谷: P1106

--- luogu/P1106 删数问题/test.py
+++ luogu/P1106 删数问题/test.py
@@ -1,20 +1,22 @@
 # P1106 删数问题 https://www.luogu.com.cn/problem/P1106
-# https://www.luogu.com.cn/record/70776430
 
 nums = list(map(int, input().strip()))
-K = int(input())
 n = len(nums)
-for _ in range(K):
-    max_diff = 10**(n - 1) * nums[0]
-    max_idx = 0
-    s = 0
-    for i in range(1, n):
-        k = n - 1 - i
-        s += 9 * nums[i - 1] * 10**k
-        diff = s + 10**k * nums[i]
-        if diff > max_diff:
-            max_idx = i
-            max_diff = diff
-    del nums[max_idx]
-    n -= 1
+k = int(input())
+i = 0
+while k:
+    if i < n - 1:
+        if nums[i] > nums[i + 1]:
+            del nums[i]
+            k -= 1
+            n -= 1
+            i = 0
+        else:
+            i += 1
+    elif i == n - 1:
+        del nums[i]
+        k -= 1
+        n -= 1
+    else:
+        i = n - 1
 print(''.join(map(str, nums)).lstrip('0') or 0)

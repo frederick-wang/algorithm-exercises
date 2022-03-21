7ecf5b683f3c508589fa036806718724eb2a854e
Zhaoji Wang <hwoam@outlook.com>
2022-03-21 12:28:38 +0800

    洛谷: P8225

--- luogu/P8225 「Wdoi-5」天才⑨与天才拆分/test.py
+++ luogu/P8225 「Wdoi-5」天才⑨与天才拆分/test.py
@@ -1,6 +1,7 @@
 # P8225 「Wdoi-5」天才⑨与天才拆分 https://www.luogu.com.cn/problem/P8225
 
+pow10 = tuple(10**k for k in range(11))
 T = int(input())
 for _ in range(T):
     k, n = map(int, input().split())
-    print('baka' if n % (10**k - 1) else 'aya')
+    print('baka' if n % (pow10[k] - 1) else 'aya')

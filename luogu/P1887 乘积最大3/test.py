4370e177de4fbc0d2ab56937c1a3988cb03f4ced
Zhaoji Wang <hwoam@outlook.com>
2022-04-08 00:33:22 +0800

    洛谷: P1887

--- luogu/P1887 乘积最大3/test.py
+++ luogu/P1887 乘积最大3/test.py
@@ -1,12 +1,6 @@
 # P1887 乘积最大3 https://www.luogu.com.cn/problem/P1887
 
 N, M = map(int, input().split())
-a = N // M
-diff = N - a * M
-nums = [a] * M
-i = M - 1
-while diff:
-    nums[i] += 1
-    i = i - 1 if i else M - 1
-    diff -= 1
+d, m = divmod(N, M)
+nums = [d] * (M - m) + [d + 1] * m
 print(' '.join(map(str, nums)))
